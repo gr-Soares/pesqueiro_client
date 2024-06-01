b5017f9f668a1b0107bc966946b8fe81f1905d0f
gr-Soares <gustavo.rg.soares@gmail.com>
2024-06-01 00:05:44 -0300

    Ajustes cards

---
+++ src/app/cliente/page.tsx
@@ -0,0 +1,9 @@
+import { CardLink } from "@/components/card";
+
+export default function Cliente() {
+    return (
+        <>
+            <CardLink href="#" image="/icons/icons8-client-48.png" title="Novo" />
+        </>
+    );
+}
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -12,14 +12,14 @@ export const CardLink = (props: CardLinkProps) => {
 
     return (
         <Link href={props.href}>
-            <div className="w-full">
-                <div className="relative w-full px-4 py-6 bg-white shadow-lg dark:bg-gray-700">
-                    <p className="text-sm font-semibold text-gray-700 border-b border-gray-200 w-max dark:text-white">
+            <div className="w-40">
+                <div className="relative w-full px-4 py-6 bg-white shadow-l">
+                    <p className="text-sm font-semibold text-gray-700 border-b border-gray-200 w-max">
                         {props.title}
                     </p>
-                    <div className="flex items-end my-6 space-x-2">
+                    <div className="flex items-center justify-center my-6 space-x-2">
                         <span>
-                            {props.image ? <Image src={props.image} alt={""} width="68" height="68" /> : <></>}
+                            {props.image ? <Image src={props.image} alt={""} width="48" height="48" /> : <></>}
                         </span>
                     </div>
                 </div>
